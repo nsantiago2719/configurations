ac324c6a22b7a0a5933214323d1e6cfad202a4f8
nsantiago2719 <nsantiago2719@gmail.com>
2019-11-27 11:28:30 +0800

    add binary, update script and i3 config

--- conky/scripts/artist.sh
+++ conky/scripts/artist.sh
@@ -3,7 +3,7 @@
 artist=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 2 "artist"|egrep -v "artist"|egrep -v "array"|cut -b 27-|cut -d '"' -f 1|egrep -v ^$`
 
 if [[ ${#artist} -gt 22 ]]; then
-  artist=$(echo $srtist| cut -c-22)
+  artist=$(echo $artist| cut -c-22)
   echo "$artist ..."
 else
   echo $artist
--- conky/scripts/title.sh
+++ conky/scripts/title.sh
@@ -2,8 +2,8 @@
 title=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 1 "title"|egrep -v "title"|cut -b 44-|cut -d '"' -f 1|egrep -v ^$`
 
 if [[ ${#title} -gt 22 ]]; then
-  song=$(echo $title | cut -c-22)
-  echo "$song ..."
+  title=$(echo $title | cut -c-22)
+  echo "$title ..."
 else
   echo $title
 fi
