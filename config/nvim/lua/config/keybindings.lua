bd510ffeddfd80563a49e0f857eb89498b14eb6a
norman santiago <nsantiago@ethernalabs.com>
2024-09-01 18:18:18 +0800

    updated keybindings

--- config/nvim/lua/config/keybindings.lua
+++ config/nvim/lua/config/keybindings.lua
@@ -5,6 +5,6 @@ vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower win
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- Buffer mappings
-vim.keymap.set('n', "<S-w>", "<cmd>BufferKill<cr>", { desc = "Close buffer" })
+vim.keymap.set('n', "<S-w>", "<cmd>bd<cr>", { desc = "Close buffer" })
 vim.keymap.set('n', "<S-l>", "<cmd>bn<cr>", { desc = "Next buffer" })
 vim.keymap.set('n', "<S-h>", "<cmd>bp<cr>", { desc = "Previous buffer" })
