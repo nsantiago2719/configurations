1ac0eb80e275e71ee662e8cd8c18e16eb22a0f1c
norman santiago <nsantiago@ethernalabs.com>
2024-09-02 13:11:43 +0800

    added dashboard and exclude for ibl

--- config/nvim/lua/plugins/plugin.lua
+++ config/nvim/lua/plugins/plugin.lua
@@ -30,7 +30,23 @@ return {
     main = "ibl",
     ---@module "ibl"
     ---@type ibl.config
-    opts = {},
+    opts = {
+      exclude = {
+        filetypes = {
+          "help",
+          'dart',
+          "startify",
+          "dashboard",
+          "packer",
+          "neogitstatus",
+          "NvimTree",
+          "Trouble",
+          "checkhealth",
+          "man",
+          "lsp",
+        },
+      },
+    },
   },
   {
     "kdheepak/lazygit.nvim",
@@ -53,6 +69,43 @@ return {
     opts = {
       -- options
     },
+  },
+  {
+    "nvimdev/dashboard-nvim",
+    lazy = false,
+    opts = function()
+      local logo = [[
+     ██╗ █████╗ ███████╗██╗     
+     ██║██╔══██╗██╔════╝██║     
+     ██║███████║█████╗  ██║     
+██   ██║██╔══██║██╔══╝  ██║     
+╚█████╔╝██║  ██║███████╗███████╗
+ ╚════╝ ╚═╝  ╚═╝╚══════╝╚══════╝
+        ]]
+      logo = string.rep("\n", 8) .. logo .. "\n\n"
+
+      local opts = {
+        theme = "doom",
+        hide = {
+          statusline = false,
+        },
+        config = {
+          header = vim.split(logo, "\n"),
+          center = {
+            { action = "ene | startinsert", desc = " New File", icon = " ", key = "n" },
+            { action = require('telescope.builtin').oldfiles, desc = " Recent Files",    icon = " ", key = "r" },
+            { action = function() vim.api.nvim_input("<cmd>qa<cr>") end, desc = " Quit", icon = " ", key = "q" },
+          },
+          footer = function()
+            local stats = require("lazy").stats()
+            local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+            return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
+          end,
+        }
+      }
+      return opts
+    end,
+
   },
 }
 
