dabe12f8925b00f3a9753e66e34abd9300dc40e9
Norman Santiago <nsantiago@ethernalabs.com>
2025-01-24 14:23:16 +0800

    fix lualine

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -3,24 +3,46 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	event = "VeryLazy",
 	config = function()
+		local colors = {
+			bg = "#202328",
+			fg = "#bbc2cf",
+			yellow = "#ECBE7B",
+			cyan = "#008080",
+			darkblue = "#081633",
+			green = "#98be65",
+			orange = "#FF8800",
+			violet = "#a9a1e1",
+			magenta = "#c678dd",
+			blue = "#51afef",
+			red = "#ec5f67",
+		}
 		require("lualine").setup({
-			optiosn = {
-				sections = {
-					lualine_z = {
-						{
-							function()
-								local schema = require("yaml-companion").get_buf_schema(0)
-								if schema.result[1].name == "none" then
-									return ""
-								end
-								return schema.result[1].name
-							end,
-						},
-						{
-							function()
-								return " " .. os.date("%R")
-							end,
-						},
+			options = {
+				component_separators = "",
+				section_separators = "",
+				theme = {
+					-- We are going to use lualine_c an lualine_x as left and
+					-- right section. Both are highlighted by c theme .  So we
+					-- are just setting default looks o statusline
+					normal = { c = { fg = colors.fg, bg = colors.bg } },
+					inactive = { c = { fg = colors.fg, bg = colors.bg } },
+				},
+			},
+			sections = {
+				lualine_z = {
+					{
+						function()
+							local schema = require("yaml-companion").get_buf_schema(0)
+							if schema.result[1].name == "none" then
+								return ""
+							end
+							return schema.result[1].name
+						end,
+					},
+					{
+						function()
+							return " " .. os.date("%R")
+						end,
 					},
 				},
 			},
