33f1fb13145d4f10d6acddaadf908c01ee9d78e5
Norman Santiago <nsantiago@ethernalabs.com>
2025-04-22 18:15:53 +0800

    Update yaml-companion and lazy-lock

--- config/nvim/lua/plugins/yaml-companion.lua
+++ config/nvim/lua/plugins/yaml-companion.lua
@@ -1,19 +1,11 @@
 return {
 	"someone-stole-my-name/yaml-companion.nvim",
-	ft = { "yaml" },
 	dependencies = {
 		{ "neovim/nvim-lspconfig" },
 		{ "nvim-lua/plenary.nvim" },
 		{ "nvim-telescope/telescope.nvim" },
 	},
-	opts = {
-		builtin_matchers = {
-			kubernetes = { enabled = true },
-		},
-	},
-	config = function(_, opts)
-		local cfg = require("yaml-companion").setup(opts)
-		require("lspconfig")["yamlls"].setup(cfg)
+	config = function()
 		require("telescope").load_extension("yaml_schema")
 	end,
 }
