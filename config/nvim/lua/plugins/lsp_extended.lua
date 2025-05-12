e7d9bd711b2bb374be3e7610e293e38aee11b571
Norman Santiago <nsantiago@ethernalabs.com>
2025-05-13 07:06:16 +0800

    nvim: update lsp signature
    lsp_signature: Update max_width and zindex

--- config/nvim/lua/plugins/lsp_extended.lua
+++ config/nvim/lua/plugins/lsp_extended.lua
@@ -14,10 +14,13 @@ return {
 		"ray-x/lsp_signature.nvim",
 		event = "InsertEnter",
 		opts = {
-			bind = true,
+			max_width = function()
+				return vim.api.nvim_win_get_width(0) * 0.5
+			end,
 			handler_opts = {
 				border = "rounded",
 			},
+			zindex = 99,
 			hint_enable = false,
 			hi_parameter = "",
 			move_signature_window_key = { "<M-j>", "<M-k>", "<M-h>", "<M-l>" },
