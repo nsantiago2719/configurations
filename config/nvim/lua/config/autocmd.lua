f57e7b6cf0b62ca5a70fa2c020671a4e26615373
Norman Santiago <nsantiago@ethernalabs.com>
2025-07-13 21:18:03 +0800

    feat: added autocmd user config

---
+++ config/nvim/lua/config/autocmd.lua
@@ -0,0 +1,12 @@
+local augroup = vim.api.nvim_create_augroup("UserConfig", {})
+
+-- Create directories when saving files
+vim.api.nvim_create_autocmd("BufWritePre", {
+	group = augroup,
+	callback = function()
+		local dir = vim.fn.expand("<afile>:p:h")
+		if vim.fn.isdirectory(dir) == 0 then
+			vim.fn.mkdir(dir, "p")
+		end
+	end,
+})
--- config/nvim/lua/config/init.lua
+++ config/nvim/lua/config/init.lua
@@ -1,3 +1,4 @@
 require("config.set")
+require("config.autocmd")
 require("config.keybindings")
 require("config.lazy")
