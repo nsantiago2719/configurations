5f34248fea7d895bcf6b12e93c124595c72a12d1
norman santiago <nsantiago2719@gmail.com>
2025-12-16 01:04:48 -0800

    feat: support neovim version 0.12 or higher

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,8 +1,8 @@
-if vim.fn.has("nvim-0.11.0") ~= 1 then
+if vim.fn.has("nvim-0.12.0") ~= 1 then
 	local message = table.concat({
 		"You are using an unsupported version of Neovim.",
 		"",
-		"This configs for nvim requires version  >= v0.11.0",
+		"This configs for nvim requires version  >= v0.12.0",
 	}, "\n")
 	vim.notify(message, vim.log.levels.ERROR)
 end
