ae2ba5e0fc0a47445936dcc5685649bc2229d972
norman santiago <nsantiago2719@gmail.com>
2025-12-23 00:03:10 -0800

    chore: update diagflow opts

--- config/nvim/lua/plugins/diagflow.lua
+++ config/nvim/lua/plugins/diagflow.lua
@@ -3,9 +3,14 @@ return {
 	"dgagn/diagflow.nvim",
 	event = "LspAttach",
 	opts = {
+		enable = function()
+			return vim.bo.filetype ~= "lazy"
+		end,
 		show_borders = true,
+		show_signs = true,
+		scope = "line",
 		format = function(diagnostic)
-			return "[LSP] " .. diagnostic.message
+			return "[Diagnostics] " .. diagnostic.message
 		end,
 	},
 }
