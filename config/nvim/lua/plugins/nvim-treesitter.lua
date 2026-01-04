cb57f6a95ad059732741e3d8cf8103757973e7d1
Norman Santiago <nsantiago@ethernalabs.com>
2026-01-04 19:52:30 +0800

    chore: update hidden keys on which-key

--- config/nvim/lua/plugins/nvim-treesitter.lua
+++ config/nvim/lua/plugins/nvim-treesitter.lua
@@ -125,8 +125,7 @@ return {
 		keys = { "<space>m", "<space>j", "<space>s" },
 		dependencies = { "nvim-treesitter/nvim-treesitter" }, -- if you install parsers with `nvim-treesitter`
 		config = function()
-			require("treesj").setup({--[[ your config ]]
-			})
+			require("treesj").setup({})
 		end,
 	},
 }
--- config/nvim/lua/plugins/which_key.lua
+++ config/nvim/lua/plugins/which_key.lua
@@ -9,14 +9,17 @@ return {
 			{ "<leader>f", group = "Search" },
 			{ "<leader>e", icon = " ", group = "NvimTree" },
 			{ "<leader>b", icon = "󰓩 ", group = "Tabs" },
-			{ "<leader>c", icon = " ", group = "C commands" },
-			{ "<leader>t", icon = " ", group = "C commands" },
+			{ "<leader>c", icon = " ", group = "Cloak" },
+			{ "<leader>t", icon = " ", group = "Terminal" },
 			{ "<leader>u", icon = " ", group = "Undo tree" },
 
 			-- ignore these keys
 			{ "<leader>Y", hidden = true },
 			{ "<leader>y", hidden = true },
 			{ "<leader>bx", hidden = true },
+			{ "<leader>j", hidden = true },
+			{ "<leader>m", hidden = true },
+			{ "<leader>s", hidden = true },
 		})
 	end,
 }
