f91d668b8d362b5f60c2b015e4a7c9d6a05e503d
Norman Santiago <nsantiago@ethernalabs.com>
2026-01-05 11:31:22 +0800

    chore: move cloak config to opts

--- config/nvim/lua/plugins/cloak.lua
+++ config/nvim/lua/plugins/cloak.lua
@@ -5,26 +5,19 @@ return {
 		{ "<leader>cp", "<cmd>CloakPreviewLine<CR>", desc = "Cloak Preview Line" },
 	},
 	lazy = false,
-	config = function()
-		require("cloak").setup({
-			enabled = true,
-			cloak_character = "*",
-			-- The applied highlight group (colors) on the cloaking, see `:h highlight`.
-			highlight_group = "Comment",
-			patterns = {
-				{
-					-- Match any file starting with ".env".
-					-- This can be a table to match multiple file patterns.
-					file_pattern = {
-						".env*",
-						"*.tfvars",
-					},
-					-- Match an equals sign and any character after it.
-					-- This can also be a table of patterns to cloak,
-					-- example: cloak_pattern = { ":.+", "-.+" } for yaml files.
-					cloak_pattern = { "=.+", ":.+" },
+	opts = {
+		enabled = true,
+		cloak_character = "*",
+		highlight_group = "Comment",
+		patterns = {
+			{
+				file_pattern = {
+					".env*",
+					"*.tfvars",
 				},
+				-- example: cloak_pattern = { ":.+", "-.+" } for yaml files.
+				cloak_pattern = { "=.+", ":.+" },
 			},
-		})
-	end,
+		},
+	},
 }
