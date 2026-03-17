5c9e0de75d30ae5fdc82194e70b116e13d58a1f4
Norman Santiago <nsantiago@ethernalabs.com>
2026-03-17 13:13:47 +0800

    chore: set snacks as picker for opencode

--- config/nvim/lua/plugins/opencode.lua
+++ config/nvim/lua/plugins/opencode.lua
@@ -4,7 +4,27 @@ return {
 		-- Recommended for `ask()` and `select()`.
 		-- Required for `snacks` provider.
 		---@module 'snacks' <- Loads `snacks.nvim` types for configuration intellisense.
-		{ "folke/snacks.nvim", opts = { input = {}, picker = {}, terminal = {} } },
+		{
+			"folke/snacks.nvim",
+			opts = {
+				input = {},
+				picker = {
+					actions = {
+						opencode_send = function(...)
+							return require("opencode").snacks_picker_send(...)
+						end,
+					},
+					win = {
+						input = {
+							keys = {
+								["<a-a>"] = { "opencode_send", mode = { "n", "i" } },
+							},
+						},
+					},
+				},
+				terminal = {},
+			},
+		},
 	},
 	config = function()
 		-- Required for `opts.events.reload`.
